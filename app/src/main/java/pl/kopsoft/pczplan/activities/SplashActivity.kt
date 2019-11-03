3c32505a681f012f371305e2a803595f6eab4e44
Patryk Kopacz <kopaczkapl@gmail.com>
2019-11-03 19:05:46 +0100

    Created basic splash screen

---
+++ app/src/main/java/pl/kopsoft/pczplan/activities/SplashActivity.kt
@@ -0,0 +1,39 @@
+package pl.kopsoft.pczplan.activities
+
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_splash.*
+import pl.kopsoft.pczplan.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val bounce = AnimatorInflater.loadAnimator(this, R.animator.anim_bounce)
+        val makeVisible = AnimatorInflater.loadAnimator(this, R.animator.anim_makevisible)
+        val grow = AnimatorInflater.loadAnimator(this, R.animator.anim_grow)
+
+        val shrinkAccent = AnimatorSet()
+        shrinkAccent.play(bounce)
+        shrinkAccent.setTarget(splash_accent)
+
+        val makeLogoVisible = AnimatorSet()
+        makeLogoVisible.play(makeVisible)
+        makeLogoVisible.startDelay = 500
+        makeLogoVisible.setTarget(splash_logo_imageview)
+
+        val growDark = AnimatorSet()
+        growDark.play(grow)
+        growDark.startDelay = 3000
+        growDark.setTarget(splash_background)
+
+        val anim = AnimatorSet()
+        anim.playTogether(shrinkAccent, makeLogoVisible, growDark)
+        anim.start()
+
+    }
+}
